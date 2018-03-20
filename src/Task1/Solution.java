7ed560659fd08ea8827b925d8a5e19a04e9311ce
KKarola <karolina.kak08@gmail.com>
2018-03-20 17:16:08 +0100

    Code refactoring

--- src/Task1/Solution.java
+++ src/Task1/Solution.java
@@ -1,130 +1,77 @@
 package Task1;
 
-public class Solution {
-    public int solution(int[] A, int[] B) {
-        int amount = 0;
-        int temp;
+class Solution {
+    private int amount = 0;
 
-        if (A.length == 2) {
-            if (A[0] >= A[1]) {
-                temp = A[1];
-                A[1] = B[1];
-                B[1] = temp;
+    private int solution(int[] arrA, int[] arrB) {
 
+        if (arrA.length == 2) {
+            if (arrA[0] >= arrA[1]) {
+                exchange(arrA, arrB, 1);
             } else {
-                if (B[0] >= B[1]) {
-                    temp = B[1];
-                    B[1] = A[1];
-                    A[1] = temp;
-                    amount++;
+                if (arrB[0] >= arrB[1]) {
+                    exchange(arrB, arrA, 1);
                 }
             }
         }
 
-        if (A.length > 2) {
-            for (int i = 1; i < A.length - 1; i++) {
-                if (A[i] < A[i - 1]) {
-                    if (A[i - 1] < B[i] && B[i] < A[i + 1]) {
-                        temp = A[i];
-                        A[i] = B[i];
-                        B[i] = temp;
-                        amount++;
-                    } else {
-                        if (A[i - 1] < B[i] && B[i] < B[i + 1]) {
-                            temp = A[i - 1];
-                            A[i - 1] = B[i - 1];
-                            B[i - 1] = temp;
-                            amount++;
-                        }
-                    }
-                } else {
-                    if (A[i] == A[i - 1]) {
-                        if (A[i - 1] < B[i] && B[i] < A[i + 1]) {
-                            temp = A[i];
-                            A[i] = B[i];
-                            B[i] = temp;
-                            amount++;
-                        } else {
-                            if (A[i - 1] < B[i] && B[i] < B[i + 1]) {
-                                temp = A[i - 1];
-                                A[i - 1] = B[i - 1];
-                                B[i - 1] = temp;
-                                amount++;
-                            }
-                        }
-                    }
+        if (arrA.length > 2) {
+            for (int i = 1; i < arrA.length - 1; i++) {
+                // checking first array
+                if (arrA[i] < arrA[i - 1] || arrA[i] == arrA[i - 1]) {
+                    exchangeElement(arrA, arrB, i);
                 }
-
-                if (B[i] < B[i - 1]) {
-                    if (B[i - 1] < A[i] && A[i] < B[i + 1]) {
-                        temp = B[i];
-                        B[i] = A[i];
-                        A[i] = temp;
-                        amount++;
-                    } else {
-                        if (B[i - 1] < A[i] && A[i] < A[i + 1]) {
-                            temp = B[i - 1];
-                            B[i - 1] = A[i - 1];
-                            A[i - 1] = temp;
-                            amount++;
-                        }
-                    }
-                } else {
-                    if (B[i] == B[i - 1]) {
-                        if (B[i - 1] < A[i] && A[i] < B[i + 1]) {
-                            temp = B[i];
-                            B[i] = A[i];
-                            A[i] = temp;
-                            amount++;
-                        } else {
-                            if (B[i - 1] < A[i] && A[i] < A[i + 1]) {
-                                temp = B[i - 1];
-                                B[i - 1] = A[i - 1];
-                                A[i - 1] = temp;
-                                amount++;
-                            }
-                        }
-                    }
-
+                // checking second array
+                if (arrB[i] < arrB[i - 1] || arrB[i] == arrB[i - 1]) {
+                    exchangeElement(arrB, arrA, i);
                 }
-
             }
-
-            if (A[A.length - 2] >= A[A.length - 1]) {
-                temp = A[A.length - 1];
-                A[A.length - 1] = B[A.length - 1];
-                B[A.length - 1] = temp;
-                amount++;
-            }
-            if (B[A.length - 2] >= B[A.length - 1]) {
-                temp = B[A.length - 1];
-                B[A.length - 1] = A[A.length - 1];
-                A[A.length - 1] = temp;
-                amount++;
-            }
+            // checking last elements
+            lastElements(arrA, arrB, arrA.length);
+            lastElements(arrB, arrA, arrA.length);
         }
-
-        for (int i = 1; i < A.length; i++) {
-            if (A[i - 1] >= A[i] || B[i - 1] >= B[i]) {
+        // checking arrays after exchange
+        for (int i = 1; i < arrA.length; i++) {
+            if (arrA[i - 1] >= arrA[i] || arrB[i - 1] >= arrB[i]) {
                 return -1;
             }
         }
 
-        if (amount > A.length / 2) {
-            amount = A.length - amount;
+        if (amount > arrA.length / 2) {
+            amount = arrA.length - amount;
         }
 
         return amount;
     }
 
+    private void exchange(int[] arrFirst, int[] arrSecond, int i) {
+        int temp = arrFirst[i];
+        arrFirst[i] = arrSecond[i];
+        arrSecond[i] = temp;
+        amount++;
+    }
+
+    private void exchangeElement(int[] arrFirst, int[] arrSecond, int i) {
+        if (arrFirst[i-1] < arrSecond[i] && arrSecond[i] < arrFirst[i+1]) {
+            exchange(arrFirst, arrSecond, i);
+        } else {
+            if (arrFirst[i-1] < arrSecond[i] && arrSecond[i] < arrSecond[i+1]) {
+                exchange(arrFirst, arrSecond, i-1);
+            }
+        }
+    }
+
+    private void lastElements(int[] arrFirst, int[] arrSecond, int len) {
+        if (arrFirst[len - 2] >= arrFirst[len - 1]) {
+            exchange(arrFirst, arrSecond, len-1);
+        }
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        int[] A = {5, 3, 7, 7, 10};
-        int[] B = {1, 6, 6, 9, 9};
-        //int[] A = {0, 3};
-        //int[] B = {2, 1};
-        int a = s.solution(A, B);
-        System.out.println(a);
+        int[] arrA = {5, 3, 7, 7, 10};
+        int[] arrB = {1, 6, 6, 9, 9};
+        System.out.println(s.solution(arrA, arrB));
     }
 }
